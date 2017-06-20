3e72ec831876f7597d3cb09dfd7253865dd7d4c6
Sidheshwar Udgire <sidheshwar.udgire@praxify.com>
2017-06-20 17:28:16 +0530

    Login page error show

--- login/login.js
+++ login/login.js
@@ -1,11 +1,28 @@
 (function (){
 	'use strict';
-
 	angular.module('dapp').controller('loginCtr', loginCtr);
+	console.log('Login');
 
 	loginCtr.$inject = ['$location'];
 	
-	function loginCtr($location){
+	function loginCtr($location, $scope, LoginService){
+	
+		var admin = 'admin';
+	    var pass = 'pass';
+	    var isAuthenticated = false;
+	    
+	    return {
+	      login : function(username, upassword) {
+	        isAuthenticated = username === admin && upassword === pass;
+	        $location.path('/login/sid.html');
+	        return isAuthenticated;
+	        console.log('Login2');
+	      },
+	      isAuthenticated : function() {
+	        $scope.error = "Incorrect username/password !";
+	        return isAuthenticated;
+	      }
+	    };
 		var login = this;
 		console.log('Login');
 	}
